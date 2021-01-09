d047d9f54aa5916cb68ec8af84ba0dba62230cc4
Michael Boniface <32021679+bmg1mike@users.noreply.github.com>
2021-01-09 10:07:04 +0100

    added bookservice

---
+++ Application/BookService.cs
@@ -0,0 +1,48 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.DTOs;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class BookService : IBookService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public BookService(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public Task<BookDto> CreateBook(BookDto book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BookDto> GetBookById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<BookDto>> GetBooks()
+        {
+            var books = await _unitOfWork.Book.ListAllAsync();
+            var booksDto = new List<BookDto>();
+            foreach (var item in books)
+            {
+                booksDto.Add(_mapper.Map<BookDto>(item));
+            }
+            return booksDto;
+        }
+
+        public Task<BookDto> UpdateBook(string id, BookDto book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Interfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IBookService
+    {
+        Task<IReadOnlyList<BookDto>> GetBooks();
+        Task<BookDto> GetBookById(string id);
+        Task<BookDto> UpdateBook(string id, BookDto book);
+        Task<BookDto> CreateBook(BookDto book);
+    }
+}
